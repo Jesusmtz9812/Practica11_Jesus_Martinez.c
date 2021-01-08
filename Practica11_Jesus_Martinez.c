ff5d088c75d47a47876ef644878c8974ec085aa7
Jesusmtz9812 <54428599+Jesusmtz9812@users.noreply.github.com>
2021-01-07 21:13:14 -0600

    Update Practica11_Jesus_Martinez.c

--- Practica11_Jesus_Martinez.c
+++ Practica11_Jesus_Martinez.c
@@ -9,7 +9,7 @@ un ciclo for.
 // funcion principal
 int main(){
     //variables locales
-    int* matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
     int i, cont=0, *ap;
         ap=matriz;
     //bloque de instrucciones
